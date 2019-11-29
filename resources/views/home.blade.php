20d2f0fd57733d1629d641917b95f1658234243c
Lilian Kempees <0926313@hr.nl>
2019-11-29 12:46:29 +0100

    Deleted profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,12 @@
             <img src="https://cat-bounce.com/catbounce.png" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div><h1>{{ Auth::user()->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>12</strong> posts</div>
                 <div class="pr-5"><strong>12</strong> posts</div>
                 <div class="pr-5"><strong>12</strong> posts</div>
             </div>
-            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
-            <div>{{ $user->profile->description }}</div>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+Route::get('/home', 'HomeController@index')->name('home');
