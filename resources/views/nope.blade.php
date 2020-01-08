a26c22ba15d809829ca43977cfb72a25787e17a6
Lilian Kempees <0926313@hr.nl>
2020-01-08 22:59:21 +0100

    Added a nope page

---
+++ resources/views/nope.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h3>The Catto said NOPE</h3>
+    <img class=img-fluid src="https://lh3.googleusercontent.com/cEQ0BQc1sk63XLqQNrZE-m213LIIyLgxGkknBX6W-euqwP4duwlfSFzN-RewTFVcxvjRBasOTUDpeys9S-OV0uoH0Pe0iwyD27Nm5-zHebnHkfohyL7tTu1zWVxzSBpLFfFvTzeb3_ZFYYTfo1h56pAPWAbVy7fMqJX5AXdllTCW8h96mLInkARKJoO6gqbOyUYtSNLU6CQ22GpCyajgChBHvwrpx8HU-oaKERp-WKSoU7vHeyUe4AVBh_LpSiOq9aLlqk_vydPmV6afn7YceLdkld8MJTiT3AlrKWKHebsMYNUqwlFA23tYLCHFdk931RVhIyW5euPJH3diP728y48EzXhBWWsqx-h6Xwx8slMtMbR9HkPxSA1soI6W6RbIIE32xP3zTcT64an3mmxBgdIVzrRJAYp69NlJNaCidsEw8_QXnWLn_tolDktK7RSnH6RU36n1J4g33MGfnilVFrvNSLkENuIevuiqS223n1Mx7qROhElxAvy1B5mashyxGjbsVuKu4SeO4zJ38SQC0SSf0OTQo2xKGWynB_2x2ZRZd3RcclQF7cQjmh5nIhYgc1A_k6i_K2PLhrx8m5-xVfZX4xnaF2ByXZKGSqk4TAxMcB-YMbCGgV0-m0WG1adn2TTT9PYQcu52tz-lCFaRYCA0jD_qWE-F7BAYwIJo9JAE3dZ9ryFong=w1224-h937-no">
+</div>
+@endsection
